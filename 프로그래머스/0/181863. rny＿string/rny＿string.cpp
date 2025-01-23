432043712303fdf55ba870d999180e07becc6747
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2025-01-23 15:41:28 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rny_string) {
+    string answer = "";
+    while(rny_string.find("m")!=string::npos){
+        rny_string.replace(rny_string.find("m"), 1, "rn");
+    }
+    return rny_string;
+}
