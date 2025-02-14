83d5359351349f6bce32914161dbb50a548bb98b
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2025-02-14 16:28:06 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer;
+    sqrt(n) == int(sqrt(n)) ? answer = 1 : answer = 2;
+    return answer;
+}
