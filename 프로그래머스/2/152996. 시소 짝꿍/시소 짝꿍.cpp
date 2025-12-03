146c365a417b918538cdece60a9df7e54e2f47eb
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2025-12-03 13:53:39 +0900

    [level 2] Title: 시소 짝꿍, Time: 8326.58 ms, Memory: 6.48 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(vector<int> weights) {
+    long long answer = 0;
+    sort(weights.begin(), weights.end(), less<int>());
+    
+    for(int i = 0; i<weights.size()-1; i++)
+    {
+        for(int j = i+1; j<weights.size(); j++)
+        {
+            if(weights[i] * 2 < weights[j]) break;
+            else if(weights[i] == weights[j]) answer++;
+            else if(weights[i] * 3 == weights[j] * 2) answer++;
+            else if(weights[i] * 2 == weights[j]) answer++;
+            else if(weights[i] * 4 == weights[j] * 3) answer++;
+        }
+    }
+    return answer;
+}
