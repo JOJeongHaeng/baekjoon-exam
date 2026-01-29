94ec5e115006fa031b46ef40374812241e8e8fb5
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2026-01-29 16:06:32 +0900

    [Silver I] Title: 포도주 시식, Time: 4 ms, Memory: 2136 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[10001];
+long long dp[10001];
+
+int main()
+{
+	int N;
+	cin >> N;
+	
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> arr[i];
+	}
+	dp[1] = arr[1];
+	dp[2] = arr[1] + arr[2];
+
+	for (int i = 3; i <= N; i++)
+	{
+		dp[i] = max(max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 1]), dp[i - 2] + arr[i]);
+	}
+	cout << dp[N];
+	return 0;
+}
