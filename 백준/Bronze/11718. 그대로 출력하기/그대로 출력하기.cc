50bcf2f7eaac9909aec16006066cb3860d850e3d
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2026-02-08 17:10:00 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	while (str != "")
+	{
+		cout << str << endl;
+		getline(cin, str);
+	}
+	
+	return 0;
+}
