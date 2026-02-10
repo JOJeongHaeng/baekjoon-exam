3b90b38fa5cd0c23cbf175a6f9cec75bd831842b
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2026-02-10 17:46:53 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[50001];
+
+int main()
+{
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		for (int j = 1; j * j <= i; j++)
+		{
+			dp[i] = min(dp[i], dp[i - j * j] + 1);
+		}
+	}
+
+	cout << dp[N];
+	return 0;
+}
